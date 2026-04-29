41898fd9c967b11b685e62a62d9b2f2fa9d128e4
Septicuss <vbalaban55@gmail.com>
2026-04-29 19:59:19 +0300

    23.

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,6 +2,9 @@ import {FlatList, View, StyleSheet, Pressable} from 'react-native';
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hook/useRepositories";
 import {useNavigate} from "react-router-native";
+import {useState} from "react";
+import Text from "./Text";
+import {Picker} from "@react-native-picker/picker";
 
 const styles = StyleSheet.create({
 	separator: {
@@ -11,7 +14,32 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories, pressRepository }) => {
+export const RepositoryListHeader = ({handleChangeOrdering}) => {
+	const [orderingType, setOrderingType] = useState('latest')
+	const orderingTypes = [
+		{id: 'latest', label: 'Latest repositories'},
+		{id: 'highest_rated', label: 'Highest rated repositories'},
+		{id: 'lowest_rated', label: 'Lowest rated repositories'}
+	]
+
+	return (
+			<>
+				<Picker
+					selectedValue={orderingType}
+					onValueChange={(itemValue) => {
+						setOrderingType(itemValue)
+						handleChangeOrdering(itemValue)
+					}}
+				>
+					{orderingTypes.map(value =>
+							<Picker.Item label={value.label} value={value.id} />
+					)}
+				</Picker>
+			</>
+	)
+}
+
+export const RepositoryListContainer = ({ repositories, pressRepository, loading, handleChangeOrdering }) => {
 	const repositoryNodes = repositories
 			? repositories.edges.map(edge => edge.node)
 			: [];
@@ -20,27 +48,62 @@ export const RepositoryListContainer = ({ repositories, pressRepository }) => {
 			<FlatList
 					data={repositoryNodes}
 					ItemSeparatorComponent={ItemSeparator}
-					renderItem={({item}) => (
-							<Pressable onPress={() => pressRepository(item)}>
-								<RepositoryItem
-										key={item.id}
-										item={item}
-								/>
-							</Pressable>
-					)}
+					ListHeaderComponent={<RepositoryListHeader handleChangeOrdering={handleChangeOrdering} />}
+					renderItem={({item}) => {
+
+						if (loading)
+							return null
+
+						return (<Pressable onPress={() => pressRepository(item)}>
+									<RepositoryItem
+											key={item.id}
+											item={item}
+									/>
+								</Pressable>
+						)
+					}}
 			/>
 	);
 }
 
 const RepositoryList = () => {
-	const { repositories } = useRepositories()
+	const [ordering, setOrdering] = useState({
+		orderBy: 'CREATED_AT',
+		orderDirection: 'DESC'
+	})
+	const { repositories, loading } = useRepositories({...ordering})
 	const navigate = useNavigate()
 
 	const pressRepository = async (repository) => {
 		navigate(`/repository/${repository.id}`)
 	}
 
-	return <RepositoryListContainer repositories={repositories} pressRepository={pressRepository} />
+	// 'latest','highest rated', 'lowest rated'
+	const handleChangeOrdering = (orderingType) => {
+		console.log('setting to', orderingType)
+		switch (orderingType.toLowerCase()) {
+			case 'latest':
+				setOrdering({orderBy: 'CREATED_AT', orderDirection: 'DESC'})
+				break
+			case 'highest_rated':
+				setOrdering({orderBy: 'RATING_AVERAGE', orderDirection: 'DESC'})
+				break
+			case 'lowest_rated':
+				setOrdering({orderBy: 'RATING_AVERAGE', orderDirection: 'ASC'})
+				break
+			default:
+				break
+		}
+		console.log(' result:',ordering)
+
+	}
+
+	return <RepositoryListContainer
+			repositories={repositories}
+			loading={loading}
+			pressRepository={pressRepository}
+			handleChangeOrdering={handleChangeOrdering}
+	/>
 };
 
 export default RepositoryList;
